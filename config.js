7e736988688a3330ae2ff2830741aa755ee85b16
Dominika Myszczynska <myszczynskad@gmail.com>
2019-05-13 13:43:39 +0100

    task done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '760078002922-esv8f9e17h382n0co2muq0m2kg381qrb.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'qVnjRPQFVglvkd0Ajm1F6MKy',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+const app = express();
+let googleProfile = {};
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(
+	new GoogleStrategy({
+		clientID: config.GOOGLE_CLIENT_ID,
+		clientSecret:config.GOOGLE_CLIENT_SECRET,
+		callbackURL: config.CALLBACK_URL
+	},
+	(accessToken, refreshToken, profile, cb) => {
+		googleProfile = {
+			id: profile.id,
+			displayName: profile.displayName
+		};
+		cb(null, profile);
+	})
+);
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static('assets'));
 
 app.use('/store', (req, res, next) => {
     console.log('Jestem pośrednikiem przy żądaniu do /store');
     next();
-});
-
-app.set('view engine', 'pug');
-app.set('views','./views');
-
-app.get('/', (req, res) => {
-    res.sendFile('/index.html');
 });
 
 app.get('/userform', (req, res) => {
@@ -38,17 +62,32 @@ app.get('/dynamic-view', (req, res) => {
     });
 });
 
-app.get('/auth', (req, res) => {
-	res.render('auth');
+app.get('/', (req, res) => {
+    res.render('index', { user: req.user });
 });
 
-app.get('/auth/google', (req, res) => {
-	res.render('auth-google');
+app.get('/logged', (req, res) => {
+    res.render('logged', { user: googleProfile });
 });
 
-var server = app.listen(3000, 'localhost', () => {
-    var host = server.address().address;
-    var port = server.address().port;
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope : ['profile', 'email']
+	})
+);
+
+app.get(
+	'/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+	})
+);
+
+const server = app.listen(3000, 'localhost', () => {
+    const host = server.address().address;
+    const port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
